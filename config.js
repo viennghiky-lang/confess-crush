899c644fd529958dc3c3c4f7842c041aeff4ca7e
viennghiky-lang <viennghiky@gmail.com>
2025-12-02 16:52:30 +0700

    Bao Nghi

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "Anh nho Bao Nghi qua",
+    introTitle: ' Tran Trinh Bao Nghi',
+    introDesc: `Thiên Hạ vốn lạ thất thường
+   cớ sao nàng lại đi nhầm đường
+    Lạc vào tim ta lẻ loi
+    một tấm chân tình tay trao nhẫn ấy ngỡ là tyeu`,
+    btnIntro: '^^KK^^',
+    title: 'Phải chăng nàng đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng nàng đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnNo: 'Không có mà :3',
+    question: 'hơn nghìn ấy ngày nay lại phải lòng tiểu thư <3',
+    btnReply: 'Gửi cho nàng <3',
+    reply: 'Yêu thì chớ ngại e dè mai sau nhớ đấy lấy gì cho thôi <33333333',
+    mess: 'Ta biết ta thương nàng nhiều ánh tà bóng nguyệt lòng ta nhớ ng',
+    messDesc: 'đêm khuya gió mát đón nàng về dinh.',
+    btnAccept: 'Dạaaa lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
